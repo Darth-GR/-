63e040712164c06028d299a397322eab7b881cd3
Darth-GR <143057895+Darth-GR@users.noreply.github.com>
2026-06-10 18:56:57 +0800

    Match CIFAR batches to model device

--- python/needle/backend_ndarray/ndarray.py
+++ python/needle/backend_ndarray/ndarray.py
@@ -591,8 +591,7 @@ class NDArray:
 
         if axis is None:
             view = self.compact().reshape((1,) * (self.ndim - 1) + (prod(self.shape),))
-            #out = NDArray.make((1,) * self.ndim, device=self.device)
-            out = NDArray.make((1,), device=self.device)
+            out = NDArray.make((), device=self.device)
 
         else:
             if isinstance(axis, (tuple, list)):
--- python/needle/init/init_initializers.py
+++ python/needle/init/init_initializers.py
@@ -3,30 +3,24 @@ from .init_basic import *
 
 
 def xavier_uniform(fan_in, fan_out, gain=1.0, **kwargs):
-    # TODO
-    ### BEGIN YOUR SOLUTION
-    raise NotImplementedError()
-    ### END YOUR SOLUTION
+    bound = gain * math.sqrt(6.0 / (fan_in + fan_out))
+    return rand(fan_in, fan_out, low=-bound, high=bound, **kwargs)
 
 
 def xavier_normal(fan_in, fan_out, gain=1.0, **kwargs):
-    # TODO
-    ### BEGIN YOUR SOLUTION
-    raise NotImplementedError()
-    ### END YOUR SOLUTION
+    std = gain * math.sqrt(2.0 / (fan_in + fan_out))
+    return randn(fan_in, fan_out, mean=0.0, std=std, **kwargs)
 
 
 def kaiming_uniform(fan_in, fan_out, shape=None, nonlinearity="relu", **kwargs):
     assert nonlinearity == "relu", "Only relu supported currently"
-    # TODO
-    ### BEGIN YOUR SOLUTION
-    raise NotImplementedError()
-    ### END YOUR SOLUTION
+    bound = math.sqrt(6.0 / fan_in)
+    shape = shape or (fan_in, fan_out)
+    return rand(*shape, low=-bound, high=bound, **kwargs)
 
 
 def kaiming_normal(fan_in, fan_out, shape=None, nonlinearity="relu", **kwargs):
     assert nonlinearity == "relu", "Only relu supported currently"
-    # TODO
-    ### BEGIN YOUR SOLUTION
-    raise NotImplementedError()
-    ### END YOUR SOLUTION
+    std = math.sqrt(2.0 / fan_in)
+    shape = shape or (fan_in, fan_out)
+    return randn(*shape, mean=0.0, std=std, **kwargs)
